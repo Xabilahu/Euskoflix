2512847a3357ab4740851d268f2ce1e65ef59ad1
Daniel Cañadillas <daniel.canadillas@outlook.com>
2019-03-08 13:47:12 +0100

    Testing messageDigest

--- Euskoflix/src/test/java/MainTest.java
+++ Euskoflix/src/test/java/MainTest.java
@@ -1,2 +1,18 @@
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class MainTest {
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        MessageDigest mg = MessageDigest.getInstance("SHA-256");
+        mg.reset();
+        mg.update("patata".getBytes());
+        BigInteger b = new BigInteger(1,mg.digest());
+        System.out.println(b.toString(16));
+        mg.reset();
+        mg.update("patata".getBytes());
+        b = new BigInteger(1,mg.digest());
+        System.out.println(b.toString(16));
+    }
+
 }
