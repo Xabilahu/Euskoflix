ee0af6fd9c56519ba9caee63a187fd2a9e4f0f17
Daniel Cañadillas <daniel.canadillas@outlook.com>
2019-04-10 20:02:04 +0200

    Some window testing

---
+++ Euskoflix/src/main/java/eus/ehu/euskoflix/packPrincipal/windowTesting/MainTesting.java
@@ -0,0 +1,19 @@
+package eus.ehu.euskoflix.packPrincipal.windowTesting;
+
+import javax.swing.*;
+
+public class MainTesting {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                SimpleSwingBrowser browser = new SimpleSwingBrowser();
+                browser.setVisible(true);
+                browser.loadURL("https://www.youtube.com/embed/QH2-TGUlwu4?autoplay=1&modestbranding=1&showinfo=0&rel=0&cc_load_policy=1&iv_load_policy=3&fs=0");
+            }
+        });
+    }
+
+}
