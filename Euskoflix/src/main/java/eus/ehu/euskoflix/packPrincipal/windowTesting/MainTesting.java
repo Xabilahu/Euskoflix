4093e067b5068be0e6b1cf65fbcca690a7f4e1e2
Daniel Cañadillas <daniel.canadillas@outlook.com>
2019-04-11 12:21:30 +0200

    Escape key binding and screen movement

--- Euskoflix/src/main/java/eus/ehu/euskoflix/packPrincipal/windowTesting/MainTesting.java
+++ Euskoflix/src/main/java/eus/ehu/euskoflix/packPrincipal/windowTesting/MainTesting.java
@@ -11,7 +11,7 @@ public class MainTesting {
             public void run() {
                 SimpleSwingBrowser browser = new SimpleSwingBrowser();
                 //browser.setVisible(true);
-                browser.loadURL("https://www.youtube.com/embed/9HfzcqeS2SU?&theme=dark&autoplay=1&autohide=1&modestbranding=1&fs=0&showinfo=0&rel=0&controls=0&disablekb=1");
+                browser.loadURL("https://www.youtube.com/embed/VaiHTvifGt0?&theme=dark&autoplay=1&autohide=1&modestbranding=1&fs=0&showinfo=0&rel=0&controls=0&disablekb=1");
             }
         });
     }
--- Euskoflix/src/main/java/eus/ehu/euskoflix/packPrincipal/windowTesting/SimpleSwingBrowser.java
+++ Euskoflix/src/main/java/eus/ehu/euskoflix/packPrincipal/windowTesting/SimpleSwingBrowser.java
@@ -1,8 +1,6 @@
 package eus.ehu.euskoflix.packPrincipal.windowTesting;
 
 import javafx.application.Platform;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.concurrent.Worker;
 import javafx.embed.swing.JFXPanel;
 import javafx.scene.Scene;
@@ -14,10 +12,9 @@ import org.w3c.dom.NodeList;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.TimerTask;
-import  java.util.Timer;
 import static javafx.concurrent.Worker.State.FAILED;
 
 public class SimpleSwingBrowser extends JFrame {
@@ -26,9 +23,10 @@ public class SimpleSwingBrowser extends JFrame {
     private final JPanel panel = new JPanel(new BorderLayout());
     private WebEngine engine;
     private String url;
-
+    private Point mousePosition;
     public SimpleSwingBrowser() {
         super();
+        mousePosition = new Point();
         initComponents();
     }
 
@@ -47,7 +45,71 @@ public class SimpleSwingBrowser extends JFrame {
         getContentPane().add(panel);
 
         this.setUndecorated(true);
-        setPreferredSize(new Dimension(1024, 600));
+        jfxPanel.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ESCAPE){
+                    jfxPanel.setVisible(false);
+                    SimpleSwingBrowser.this.dispose();
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+
+            }
+        });
+        jfxPanel.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                mousePosition.x = e.getX();
+                mousePosition.y = e.getY();
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+
+
+        });
+
+        jfxPanel.addMouseMotionListener(new MouseMotionListener() {
+
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                int x = e.getX();
+                int y = e.getY();
+                int windowX = SimpleSwingBrowser.this.getLocation().x;
+                int windowY = SimpleSwingBrowser.this.getLocation().y;
+                SimpleSwingBrowser.this.move(windowX + (x - mousePosition.x), windowY + (y - mousePosition.y));
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e) {
+            }
+        });
+        setPreferredSize(new Dimension(640, 360));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         pack();
         requestFocus();
@@ -63,58 +125,52 @@ public class SimpleSwingBrowser extends JFrame {
                 engine = view.getEngine();
                 engine.getLoadWorker()
                         .exceptionProperty()
-                        .addListener(new ChangeListener<Throwable>() {
-                            @Override
-                            public void changed(ObservableValue<? extends Throwable> o, Throwable old, final Throwable value) {
-                                if (engine.getLoadWorker().getState() == FAILED) {
-                                    System.out.println("error");
-                                    /*SwingUtilities.invokeLater(new Runnable() {
-                                        @Override
-                                        public void run() {
-                                            JOptionPane.showMessageDialog(
-                                                    panel,
-                                                    (value != null)
-                                                            ? engine.getLocation() + "\n" + value.getMessage()
-                                                            : engine.getLocation() + "\nUnexpected error.",
-                                                    "Loading error...",
-                                                    JOptionPane.ERROR_MESSAGE);
-                                        }
-                                    });*/
-                                }
+                        .addListener((o, old, value) -> {
+                            if (engine.getLoadWorker().getState() == FAILED) {
+                                System.out.println("error");
+                                /*SwingUtilities.invokeLater(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        JOptionPane.showMessageDialog(
+                                                panel,
+                                                (value != null)
+                                                        ? engine.getLocation() + "\n" + value.getMessage()
+                                                        : engine.getLocation() + "\nUnexpected error.",
+                                                "Loading error...",
+                                                JOptionPane.ERROR_MESSAGE);
+                                    }
+                                });*/
                             }
                         });
-                engine.getLoadWorker().stateProperty().addListener(new ChangeListener<Worker.State>() {
-                    @Override
-                    public void changed(ObservableValue<? extends Worker.State> observable, Worker.State oldValue, Worker.State newValue) {
-                        if (newValue == Worker.State.SUCCEEDED){
-                            Document d = engine.getDocument();
-                            NodeList nl = d.getElementsByTagName("div");
-                            for (int i = 0 ; i < nl.getLength(); i++){
-                                Element e = (Element) nl.item(i);
-                                String classes = e.getAttribute("class");
-                                if(classes != null &&
-                                        (classes.contains("ytp-chrome-top")
-                                        || classes.contains("ytp-gradient-bottom")
-                                        || classes.contains("ytp-gradient-top")
-                                        || classes.contains("ytp-chrome-bottom")
-                                        )){
-                                    e.setAttribute("style","display:none !important");
-                                    e.setTextContent("");
-                                }else if(classes != null && classes.contains("ytp-pause-overlay ")){
-                                    e.setTextContent("");
-                                    e.setAttribute("class","");
-                                }
-                            }
-                            nl = d.getElementsByTagName("a");
-                            for (int i = 0 ; i < nl.getLength(); i++){
-                                Element e = (Element) nl.item(i);
-                                e.setAttribute("href","#");
+                engine.getLoadWorker().stateProperty().addListener((observable, oldValue, newValue) -> {
+                    if (newValue == Worker.State.SUCCEEDED){
+                        Document d = engine.getDocument();
+                        NodeList nl = d.getElementsByTagName("div");
+                        for (int i = 0 ; i < nl.getLength(); i++){
+                            Element e = (Element) nl.item(i);
+                            String classes = e.getAttribute("class");
+                            if(classes != null &&
+                                    (classes.contains("ytp-chrome-top")
+                                    || classes.contains("ytp-gradient-bottom")
+                                    || classes.contains("ytp-gradient-top")
+                                    || classes.contains("ytp-chrome-bottom")
+                                    )){
+                                e.setAttribute("style","display:none !important");
                                 e.setTextContent("");
+                            }else if(classes != null && classes.contains("ytp-pause-overlay ")){
+                                e.setTextContent("");
+                                e.setAttribute("class","");
                             }
-                            SimpleSwingBrowser.this.setVisible(true);
-                        }else if(oldValue == Worker.State.SUCCEEDED){
-                            loadURL(SimpleSwingBrowser.this.url);
                         }
+                        nl = d.getElementsByTagName("a");
+                        for (int i = 0 ; i < nl.getLength(); i++){
+                            Element e = (Element) nl.item(i);
+                            e.setAttribute("href","#");
+                            e.setTextContent("");
+                        }
+                        SimpleSwingBrowser.this.setVisible(true);
+                    }else if(oldValue == Worker.State.SUCCEEDED){
+                        loadURL(SimpleSwingBrowser.this.url);
                     }
                 });
                 jfxPanel.setScene(new Scene(view));
@@ -124,17 +180,14 @@ public class SimpleSwingBrowser extends JFrame {
 
     public void loadURL(final String url) {
         this.url = url;
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                String tmp = toURL(url);
+        Platform.runLater(() -> {
+            String tmp = toURL(url);
 
-                if (tmp == null) {
-                    tmp = toURL("http://" + url);
-                }
-
-                engine.load(tmp);
+            if (tmp == null) {
+                tmp = toURL("http://" + url);
             }
+
+            engine.load(tmp);
         });
     }
 }
